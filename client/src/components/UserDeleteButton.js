b6d71fedb17735df1aeaed31440b471937e660b3
Taylor Thompson <taylor@nightshiftwaxco.com>
2022-08-05 04:47:42 -0400

    removed dead components... was a great learning experience! No more prop drilling though.

--- client/src/components/UserDeleteButton.js
+++
@@ -1,9 +0,0 @@
-function UserDeleteButton({ id, deleteUser }) {
-  return (
-    <button className="btn" type="button" onClick={() => deleteUser(id)}>
-      Delete User
-    </button>
-  );
-}
-
-export default UserDeleteButton;
